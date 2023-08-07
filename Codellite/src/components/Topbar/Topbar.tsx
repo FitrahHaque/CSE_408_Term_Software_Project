f77c634c61b92caeee335612ba40f63b68009845
Asif Ihtemadul Haque <asifihtemadulhaque@gmail.com>
2023-08-07 07:53:30 +0600

    dropdown done

--- Codellite/src/components/Topbar/Topbar.tsx
+++ Codellite/src/components/Topbar/Topbar.tsx
@@ -13,13 +13,13 @@ import { useRouter } from "next/router";
 import { Button } from "@/components/ui/button";
 import { Icons } from "@/components/icons"
 import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
+	NavigationMenu,
+	NavigationMenuContent,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	NavigationMenuTrigger,
+	navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
 
 type TopbarProps = {
@@ -27,43 +27,43 @@ type TopbarProps = {
 };
 const components: { title: string; href: string; description: string }[] = [
 	{
-	  title: "Alert Dialog",
-	  href: "/docs/primitives/alert-dialog",
-	  description:
-		"A modal dialog that interrupts the user with important content and expects a response.",
+		title: "Alert Dialog",
+		href: "/docs/primitives/alert-dialog",
+		description:
+			"A modal dialog that interrupts the user with important content and expects a response.",
 	},
 	{
-	  title: "Hover Card",
-	  href: "/docs/primitives/hover-card",
-	  description:
-		"For sighted users to preview content available behind a link.",
+		title: "Hover Card",
+		href: "/docs/primitives/hover-card",
+		description:
+			"For sighted users to preview content available behind a link.",
 	},
 	{
-	  title: "Progress",
-	  href: "/docs/primitives/progress",
-	  description:
-		"Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+		title: "Progress",
+		href: "/docs/primitives/progress",
+		description:
+			"Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
 	},
 	{
-	  title: "Scroll-area",
-	  href: "/docs/primitives/scroll-area",
-	  description: "Visually or semantically separates content.",
+		title: "Scroll-area",
+		href: "/docs/primitives/scroll-area",
+		description: "Visually or semantically separates content.",
 	},
 	{
-	  title: "Tabs",
-	  href: "/docs/primitives/tabs",
-	  description:
-		"A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+		title: "Tabs",
+		href: "/docs/primitives/tabs",
+		description:
+			"A set of layered sections of content—known as tab panels—that are displayed one at a time.",
 	},
 	{
-	  title: "Tooltip",
-	  href: "/docs/primitives/tooltip",
-	  description:
-		"A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+		title: "Tooltip",
+		href: "/docs/primitives/tooltip",
+		description:
+			"A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
 	},
-  ]
-   
-  
+]
+
+
 const Topbar: React.FC<TopbarProps> = ({ problemPage }) => {
 	const [user] = useAuthState(auth);
 	const setAuthModalState = useSetRecoilState(authModalState);
@@ -100,7 +100,69 @@ const Topbar: React.FC<TopbarProps> = ({ problemPage }) => {
 				)}
 
 				<div className='flex items-center space-x-4 flex-1 justify-end'>
-					<div>
+					<NavigationMenu>
+						<NavigationMenuList>
+							<NavigationMenuItem>
+								<NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+								<NavigationMenuContent>
+									<ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+										<li className="row-span-3">
+											<NavigationMenuLink asChild>
+												<a
+													className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b 
+													from-slate-500 to-slate-900 p-6 no-underline outline-none focus:shadow-md"
+													href="/"
+												>
+													<Icons.logo className="h-6 w-6" />
+													<div className="mb-2 mt-4 text-lg font-medium">
+														shadcn/ui
+													</div>
+													<p className="text-sm leading-tight text-muted-foreground">
+														Beautifully designed components built with Radix UI and
+														Tailwind CSS.
+													</p>
+												</a>
+											</NavigationMenuLink>
+										</li>
+										<Link href="/docs" title="Introduction">
+											Re-usable components built using Radix UI and Tailwind CSS.
+										</Link>
+										<Link href="/docs/installation" title="Installation">
+											How to install dependencies and structure your app.
+										</Link>
+										<Link href="/docs/primitives/typography" title="Typography">
+											Styles for headings, paragraphs, lists...etc
+										</Link>
+									</ul>
+								</NavigationMenuContent>
+							</NavigationMenuItem>
+							<NavigationMenuItem>
+								<NavigationMenuTrigger>Components</NavigationMenuTrigger>
+								<NavigationMenuContent>
+									<ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+										{components.map((component) => (
+											<Link
+												key={component.title}
+												title={component.title}
+												href={component.href}
+											>
+												{component.description}
+											</Link>
+										))}
+									</ul>
+								</NavigationMenuContent>
+							</NavigationMenuItem>
+							<NavigationMenuItem>
+								<Link href="/docs" legacyBehavior passHref>
+									<NavigationMenuLink className={navigationMenuTriggerStyle()}>
+										Student
+									</NavigationMenuLink>
+								</Link>
+							</NavigationMenuItem>
+						</NavigationMenuList>
+					</NavigationMenu>
+
+					{/* <div>
 						<a
 							href='https://www.google.com'
 							target='_blank'
@@ -109,7 +171,7 @@ const Topbar: React.FC<TopbarProps> = ({ problemPage }) => {
 						>
 							Student
 						</a>
-					</div>
+					</div> */}
 					{problemPage && <Timer />}
 					{!user && (
 						<Link
@@ -118,7 +180,7 @@ const Topbar: React.FC<TopbarProps> = ({ problemPage }) => {
 						>
 							<button className='bg-dark-fill-3 py-1 px-2 cursor-pointer rounded '>Sign In</button>
 						</Link>
-					)} 
+					)}
 					{user && (
 						<div className='cursor-pointer group relative'>
 							<Image src='/avatar.png' alt='Avatar' width={30} height={30} className='rounded-full' />
