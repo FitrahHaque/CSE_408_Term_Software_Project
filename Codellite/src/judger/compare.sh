c2a0a64dca0d045a21002597afc4864df1ab560d
neo11235 <1805045@ugrad.cse.buet.ac.bd>
2023-09-09 16:39:29 +0600

    modified compare script

--- Codellite/src/judger/compare.sh
+++ Codellite/src/judger/compare.sh
@@ -3,5 +3,12 @@ if [ ${REPLY} -eq 0 ]
 then
          echo "1"
 else
-         echo "0"
+         echo "0" 
 fi
+#echo "test"
+# if diff -q $1 $2; then 
+# echo "1"
+# echo diff $1 $2 > tmp.txt
+# else
+# echo "0"
+# fi
