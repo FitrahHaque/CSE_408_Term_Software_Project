6582594c2741cb0e79a516c06f9603abe91ed9e9
neo11235 <1805045@ugrad.cse.buet.ac.bd>
2023-09-09 23:33:29 +0600

    handled error message

--- Codellite/src/judger/judger.tsx
+++ Codellite/src/judger/judger.tsx
@@ -3,7 +3,7 @@ var child_process = require('child_process');
 // var fs = require('fs');
 // const { type } = require('os');
 // 
-function execute(command:string) {
+function execute1(command:string) {
     /**
      * @param {Function} resolve A function that resolves the promise
      * @param {Function} reject A function that fails the promise
@@ -26,7 +26,7 @@ function execute(command:string) {
         }
   
         if (standardError) {
-           console.log(standardError);
+        console.log(standardError);
           reject(standardError);
   
           return;
@@ -36,30 +36,77 @@ function execute(command:string) {
       });
     });
   }
+function gccparser(errormsg:string){
+    return errormsg;
+}
+function execute(command:string) {
+   
+      let res:string = "unassigned";
+      try{
+       child_process.execSync(command, function(error:any, standardOutput:any, standardError:any) {
+        
+        if (error) {
+            console.log("insid child");
+            console.log(error);
+            res = error.toString();
+            console.log(res);
+        //   reject();
+  
+          return;
+        }
+  
+        if (standardError) {
+            console.log(standardError);
+        //   reject(standardError);
+            console.log("standard error");
+          return;
+        }
+        // resolve(standardOutput);
+      });
+      console.log("successfully compiled");
+      res = "success";
 
+    }catch(exception){
+        console.log("here i am ");
+        console.log("clog ", exception.toString());
+        return gccparser(exception.toString());
+    }
+    return res;
+  }
 const run_code_cpp = async (filepath:string,filename:string, testFilePathFull:string, testOutputFilePathFull:string) => {
+    let compile:any = "not assigned yet";
     try{
-        const compile = await execute(`g++ ${filepath}/${filename}.cpp -o ./src/judger/${filename}.exec`);
-        console.log("Compilation Successful");
+        compile = execute(`g++ ${filepath}/${filename}.cpp -o ./src/judger/${filename}.exec`);
+        if(compile != "success"){
+            console.log(compile);
+            console.log("Complilation Error");
+            const res:boolean = false;
+            const str:string = compile.toString();
+            //console.log("Error: ", str);
+            return {res, str};
+        }
+        else{
+            console.log("Compilation Successful");
+        }
     }catch(error){
-        console.log(error)
-        console.log("Complilation Error");
+        //console.log(error)
+        console.log("Complilation Error here");
         const res:boolean = false;
-        const str:string = error as string;
-        console.log("Error: ", str);
+        const str:string = compile;
+        //console.log("Error: ", str);
         return {res, str};
     }
     try{
         let pass = process.env.OS_PASS;
         
-        const run = await execute(`echo ${pass} | sudo -S  ./src/judger/libjudger.so --max_cpu_time=100 --max_real_time=1000 --max_memory=130023424 --exe_path=./src/judger/${filename}.exec --input_path=${testFilePathFull} --output_path=./src/judger/${filename}.out --error_path=./src/judger/error.out`);
+        const run = await execute1(`echo ${pass} | sudo -S  ./src/judger/libjudger.so --max_cpu_time=100 --max_real_time=1000 --max_memory=130023424 --exe_path=./src/judger/${filename}.exec --input_path=${testFilePathFull} --output_path=./src/judger/${filename}.out --error_path=./src/judger/error.out`);
         console.log("Runtime successfull");
-
+        console.log("run1 " , run);
         const result = JSON.parse(run as string);
        
         if(result.result === 0){
             console.log(`./src/judger/compare.sh ${testOutputFilePathFull} ./src/judger/${filename}.out`);
-            let checkR = await execute(`./src/judger/compare.sh ${testOutputFilePathFull} ./src/judger/${filename}.out`);
+            let checkR = await execute1(`./src/judger/compare.sh ${testOutputFilePathFull} ./src/judger/${filename}.out`);
             console.log("checkR: ", checkR);
             if(parseInt(checkR as string) === 1){
                 console.log("here");
